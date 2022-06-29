c0be11da519d770377271b75e84d3bc8646eea42
Ankit Raj <ardeorajankit@gmail.com>
2022-06-29 20:50:37 +0530

    Initial commit

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,11 +1,11 @@
-var x=0;
-var y=1;
+var fib1=0;
+var fib2=1;
 var temp=0;
-console.log(x);
-while(y<5000)
+console.log(fib1);
+while(fib2<5000)
 {
-    console.log(y);
-    temp=x;
-    x=y;
-    y=temp+y;
+    console.log(fib2);
+    temp=fib1;
+    fib1=fib2;
+    fib2=temp+fib1;
 }
